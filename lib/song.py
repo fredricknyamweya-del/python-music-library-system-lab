cde9e8b6bdcbc0d91affe3628c5650faa4b23348
Fredrick Nyamweya <fredrick.nyamweya@student.moringaschool.com>
2026-06-06 12:17:42 +0300

    Initial Commit

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,57 @@
 class Song:
-    pass
+    # --- Class Attributes ---
+    count = 0
+    genres = set()
+    artists = set()
+    genre_count = {}
+    artists_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        # Trigger class methods on creation
+        Song.add_song_to_count()
+        Song.add_to_genres(genre)
+        Song.add_to_artists(artist)
+        Song.add_to_genre_count(genre)
+        Song.add_to_artists_count(artist)
+
+    # --- Class Methods ---
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        cls.genres.add(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        cls.artists.add(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    @classmethod
+    def add_to_artists_count(cls, artist):
+        if artist in cls.artists_count:
+            cls.artists_count[artist] += 1
+        else:
+            cls.artists_count[artist] = 1
+
+    # --- Utility Method ---
+    @classmethod
+    def library_summary(cls):
+        return {
+            "Total Songs": cls.count,
+            "Artists": list(cls.artists),
+            "Genres": list(cls.genres),
+            "Genre Count": cls.genre_count,
+            "Artists Count": cls.artists_count
+        }
